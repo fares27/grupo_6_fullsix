c3b6133963f065ac2990655fd4d47c342b9f34de
fares27 <amir.bartilone@gmail.com>
2022-12-02 19:39:28 -0300

    Subo estructura de carpetas y archivos. Actualizo APP.js para que levante servidor y requires iniciales

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require ('express');
+const path = require('path');
+const app = express();
+
+// Crear el path hacia la carpeta Public
+const publicPath = path.resolve(__dirname,'./public');
+
+// Establecer como carpeta Publica - Public
+app.use(express.static(publicPath));
+
+// Poner a escuchar el servidor en el puerto indicado
+app.listen(3000, () => {
+console.log ("Servidor corriendo en el puerto 3000")
+});
+
+// Vista a ejecutar en el Home
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/.html'))
+})
