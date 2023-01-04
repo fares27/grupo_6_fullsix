c1ff4b1f5cd1a1d6d43d412ac40db34089cd1fd2
Marcos923 <marcos.daut@gmail.com>
2023-01-04 19:36:16 -0300

    agreando EJS al proyecto

--- app.js
+++ app.js
@@ -1,35 +1,37 @@
-const express = require ('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 
 // Crear el path hacia la carpeta Public
-const publicPath = path.resolve(__dirname,'./public');
+const publicPath = path.resolve(__dirname, "./public");
 
 // Establecer como carpeta Publica - Public
 app.use(express.static(publicPath));
 
+app.set("view engine", "ejs");
+
 // Poner a escuchar el servidor en el puerto indicado
 app.listen(3000, () => {
-console.log ("Servidor corriendo en el puerto 3000")
+  console.log("Servidor corriendo en el puerto 3000");
 });
 
 // Vista a ejecutar en el Home
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/index.html'))
-})
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-app.get('/login', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/login.html'))
-})
+app.get("/login", (req, res) => {
+  res.render("login");
+});
 
-app.get('/productCart', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/productCart.html'))
-})
+app.get("/productCart", (req, res) => {
+  res.render("productCart");
+});
 
-app.get('/productDetail', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/productDetail.html'))
-})
+app.get("/productDetail", (req, res) => {
+  res.render("productDetail");
+});
 
-app.get('/register', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/register.html'))
-})
+app.get("/register", (req, res) => {
+  res.render("register");
+});
