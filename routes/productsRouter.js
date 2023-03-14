07c5d3c7274d4f013e3ca4f177f3b858400445f3
jazminfarias <jazminfarias07@gmail.com>
2023-03-13 21:31:24 -0300

    nada nuevo

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const router = express.Router();
-const productsController = require("../controllers/productsController")
+const productsController = require("../controllers/productsController");
 const path = require("path");
 
 // MIDDLEWARE PARA VERIFICAR SI EL USUARIO ESTA AUTENTICADO
