de230ade1ec7c8d41a2dfbe53836ef409c14d47b
fares27 <amir.bartilone@gmail.com>
2023-04-10 21:23:10 -0300

    Agrego al CRUD de Productos que tome la info de la Base de Datos: Create, Edit, Delete

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -14,114 +14,178 @@ productCart: (req, res) => {
     res.render("./products/productCart");
 },
 
+// OBTENIDO DE BASE DE DATOS
 products: (req, res) => {
 
-    const listadoPlanesJson = fs.readFileSync(path.join(__dirname, '../data/products.json'));
-    const listadoPlanes = JSON.parse(listadoPlanesJson);
+    db.Product.findAll({
+        include: [{association: 'productCategory'}]
+    })
+    .then(productos => {
+        res.render("./products/productDetail", {planes:productos});
+    })
+  //  const listadoPlanesJson = fs.readFileSync(path.join(__dirname, '../data/products.json'));
+  //  const listadoPlanes = JSON.parse(listadoPlanesJson);
 
-    let listadoPlanesActivos = listadoPlanes.filter(plan => plan.state == 1);
+  // let listadoPlanesActivos = listadoPlanes.filter(plan => plan.state == 1);
 
-    res.render("./products/productDetail", {planes:listadoPlanesActivos});
 },
 
+ //OBTENIDO DE LA BASE DE DATOS
 productDetail: (req, res) => {
     
-    const listadoPlanesJson = fs.readFileSync(path.join(__dirname, '../data/products.json'));
-    const listadoPlanes = JSON.parse(listadoPlanesJson);
+// const listadoPlanesJson = fs.readFileSync(path.join(__dirname, '../data/products.json'));
+// const listadoPlanes = JSON.parse(listadoPlanesJson);
 
     let id = req.params.id;
 
-    let planEncontrado = listadoPlanes.filter(plan => plan.id == id);
+//    let planEncontrado = listadoPlanes.filter(plan => plan.id == id);
 
-    res.render("./products/productDetail", {planes:planEncontrado});
+    db.Product.findByPk(id)
+    .then(producto => {
+        // LO MANDO COMO ARRAY PORQUE REUTILIZO LA VISTA DE DETALLE Y ESPERA REALIZAR UN FOREACH SOBRE UN ARRAY.
+        let listaProducto = [];
+        listaProducto.push(producto);
+        res.render("./products/productDetail", {planes:listaProducto});
+        
+    })
+  // res.json(planEncontrado);
+   //res.render("./products/productDetail", {planes:planEncontrado});
 },
 
 productFormLoad: (req, res) => {
     res.render("./products/productLoad");
 },
 
+// PENDIENTE DE MODIFICAR
 productFormEdit: (req, res) => {
     let id = req.params.id;
 
-    const listadoPlanesJson = fs.readFileSync(path.join(__dirname, '../data/products.json'));
-    const listadoPlanes = JSON.parse(listadoPlanesJson);
+//    const listadoPlanesJson = fs.readFileSync(path.join(__dirname, '../data/products.json'));
+//    const listadoPlanes = JSON.parse(listadoPlanesJson);
 
-    let planEncontrado = listadoPlanes.filter(plan => plan.id == id);
+//    let planEncontrado = listadoPlanes.filter(plan => plan.id == id);
 
     // console.log(planEncontrado);
-    res.render("./products/productEdit",{plan:planEncontrado});
+//    res.render("./products/productEdit",{plan:planEncontrado});
+
+    db.Product.findByPk(id)
+    .then(producto => {
+        // LO MANDO COMO ARRAY PORQUE REUTILIZO LA VISTA DE DETALLE Y ESPERA REALIZAR UN FOREACH SOBRE UN ARRAY.
+        let listaProducto = [];
+        listaProducto.push(producto);
+  //      res.render("./products/productDetail", {planes:listaProducto});
+            res.render("./products/productEdit",{plan:listaProducto});
+        
+        })
 },
 
+// CREADO EN LA BASE DE DATOS
 productCreate: (req, res) => {
+        //req.body.image = req.file.filename;
+        //return res.send(req.body);
+        const _body = { 
+            name : req.body.name,
+            description: req.body.description,
+            duration: req.body.duration,
+            image: "",
+            id_category : req.body.category,
+            price : req.body.price
+        }    
+        //return res.send(_body);
+        Product.create(_body)
+        .then(plan =>{
+            res.redirect('/products');
+        })
+ //   const listadoPlanesJson = fs.readFileSync(path.join(__dirname, '../data/products.json'));
 
-    const listadoPlanesJson = fs.readFileSync(path.join(__dirname, '../data/products.json'));
+//    const listadoPlanes = JSON.parse(listadoPlanesJson);
 
-    const listadoPlanes = JSON.parse(listadoPlanesJson);
+//planToCreate = {
+//        id: listadoPlanes[listadoPlanes.length-1].id + 1,
+//        ...req.body,
+//        state: 1
+//    }
 
-    planToCreate = {
-        id: listadoPlanes[listadoPlanes.length-1].id + 1,
-        ...req.body,
-        state: 1
-    }
-
-    listadoPlanes.push(planToCreate)
+  //  listadoPlanes.push(planToCreate)
 
    // console.log(listadoPlanes);
-    const newListadoPlanes = JSON.stringify(listadoPlanes);
+//    const newListadoPlanes = JSON.stringify(listadoPlanes);
 
-     fs.writeFileSync(path.join(__dirname, '../data/products.json'),newListadoPlanes,'');
+  //   fs.writeFileSync(path.join(__dirname, '../data/products.json'),newListadoPlanes,'');
     
   
-    res.redirect("/");
+   // res.redirect("/");
 },
 
+// PENDIENTE DE MODIFICAR
 productEdit: (req, res) => {
 
     let id = req.params.id;
 
-    const listadoPlanesJson = fs.readFileSync(path.join(__dirname, '../data/products.json'));
+ //   const listadoPlanesJson = fs.readFileSync(path.join(__dirname, '../data/products.json'));
 
-    const listadoPlanes = JSON.parse(listadoPlanesJson);
+ //   const listadoPlanes = JSON.parse(listadoPlanesJson);
 
-    planToEdit = {
-        id: id,
-        ...req.body,
-        state: 1
-    }
+ //   planToEdit = {
+ //       id: id,
+ //       ...req.body,
+ //       state: 1
+ //   }
+
+    Product.update ({
+        name : req.body.name,
+        description: req.body.description,
+        duration: req.body.duration,
+        image: "",
+        id_category : req.body.category,
+        price : req.body.price
+    }, {
+        where: {
+            id:req.params.id
+       }
+    })
+    .then(()=> res.redirect('/products/'+req.params.id+'/'))
  
-    console.log(planToEdit);
+//    console.log(planToEdit);
     
-    const listadoPlanesActualizado = listadoPlanes.map(plan => plan.id == planToEdit.id ? planToEdit: plan );
+//   const listadoPlanesActualizado = listadoPlanes.map(plan => plan.id == planToEdit.id ? planToEdit: plan );
 
 
-    const newListadoPlanesActualizado = JSON.stringify(listadoPlanesActualizado);
+//    const newListadoPlanesActualizado = JSON.stringify(listadoPlanesActualizado);
 
     
-     fs.writeFileSync(path.join(__dirname, '../data/products.json'),newListadoPlanesActualizado,'');
+//     fs.writeFileSync(path.join(__dirname, '../data/products.json'),newListadoPlanesActualizado,'');
     
-    res.redirect("/");
+//    res.redirect("/");
 },
 
+// PENDIENTE DE MODIFICAR
 productDelete: (req, res) => {
-    let id = req.params.id;
+    //let id = req.params.id;
 
-    const listadoPlanesJson = fs.readFileSync(path.join(__dirname, '../data/products.json'));
+    //const listadoPlanesJson = fs.readFileSync(path.join(__dirname, '../data/products.json'));
 
-    const listadoPlanes = JSON.parse(listadoPlanesJson);
+    //const listadoPlanes = JSON.parse(listadoPlanesJson);
 
-    const planAActualizar = listadoPlanes.filter(plan => plan.id == id);
+    //const planAActualizar = listadoPlanes.filter(plan => plan.id == id);
 
-    planAActualizar[0].state = 0;
+    //planAActualizar[0].state = 0;
 
-    const listadoPlanesActualizado = listadoPlanes.map(plan => plan.id == planAActualizar.id ? planAActualizar: plan );
+    //const listadoPlanesActualizado = listadoPlanes.map(plan => plan.id == planAActualizar.id ? planAActualizar: plan );
 
-    const newListadoPlanesActualizado = JSON.stringify(listadoPlanesActualizado);
+    //const newListadoPlanesActualizado = JSON.stringify(listadoPlanesActualizado);
     
-    fs.writeFileSync(path.join(__dirname, '../data/products.json'),newListadoPlanesActualizado,'');
+    //fs.writeFileSync(path.join(__dirname, '../data/products.json'),newListadoPlanesActualizado,'');
 
-    res.redirect("/");
+    //res.redirect("/");
+    Product.destroy({
+        where: {
+            id : req.params.id
+        }
+    })
+    .then(()=>  res.redirect('/products'))
 },
-
+// CREADO PARA PROBAR EL SEQUELIZE
 productAll:  (req, res) => {
     db.Product.findAll({
         include: [{association: 'productCategory'}]
@@ -130,7 +194,7 @@ productAll:  (req, res) => {
         res.json(productos);
     })
 },
-
+// CREADO PARA PROBAR EL SEQUELIZE
 cartAll: (req, res) => {
     db.Cart.findAll({
         include: [{association: 'user'}, {association: 'productCarts'}]
