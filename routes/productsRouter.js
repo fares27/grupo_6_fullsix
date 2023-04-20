1989660b06ddb6659f971ae70f6c9d56c3f6e0ea
fares27 <amir.bartilone@gmail.com>
2023-04-19 22:27:58 -0300

    Agrego middlewares de control de acceso de administrador a las vistas correspondientes.

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -25,21 +25,32 @@ const authMiddleware = (req, res, next) => {
     next();
   };
 
+  const authMiddlewareAdmin = (req, res, next) => {
+   if (!req.session.email) {
+      res.status(404).render('not-found')
+   }
+   else if (req.session.email.id_rol  !== 1) {
+      res.status(404).render('not-found')
+   }
+   next();
+ };
+
 
 
 router.get("/productCart", authMiddleware,productsController.productCart); // CARRITO DE COMPRAS
 
-router.get("/products/create", productsController.productFormLoad); // FORMULARIO DE CREACION DE PRODUCTO // CREATE GET
+router.get("/products/create",authMiddlewareAdmin, productsController.productFormLoad); // FORMULARIO DE CREACION DE PRODUCTO // CREATE GET
 
-//router.post("/products", productsController.productCreate); // FORMULARIO DE ENVIO DE CREACION DE PRODUCTO // CREATE POST
 router.post("/products",uploadFileProduct.single('image'), productsController.productCreate); // FORMULARIO DE ENVIO DE CREACION DE PRODUCTO // CREATE POST
 
-router.get("/products", authMiddleware,productsController.products); // LISTADO DE PLANES (PRODUCTOS) // READ ALL
+router.get("/products",productsController.products); // LISTADO DE PLANES (PRODUCTOS) // READ ALL
 
 router.get("/products/:id/", productsController.productDetail); //  LISTADO DE PLAN (PRODUCTO) // READ ONLY 
-router.get("/products/:id/edit", productsController.productFormEdit); // FORMULARIO DE EDICION DE PRODUCTO // UPDATE GET
+router.get("/products/:id/edit",authMiddlewareAdmin, productsController.productFormEdit); // FORMULARIO DE EDICION DE PRODUCTO // UPDATE GET
 router.put("/products/:id/", productsController.productEdit); // FORMULARIO DE ENVIO DE EDICION DE PRODUCTO // UPDATE PUT
 router.delete("/products/:id/", productsController.productDelete); // FORMULARIO DE ENVIO DE ORDEN DE ELIMINACION
+
+// PARA PRUEBAS
 router.get("/productsAll", productsController.productAll); // Prueba de traerme todos los productos
 router.get("/cartsAll", productsController.cartAll); // Prueba de traerme todos los productos
 module.exports = router;
