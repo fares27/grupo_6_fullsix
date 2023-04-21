4d19ce674bd437ac85b99fa15d8063595e56ac5d
Marcos923 <marcos.daut@gmail.com>
2023-04-21 19:42:16 -0300

    cambio config

--- data/config/config.js
+++ data/config/config.js
@@ -1,27 +1,26 @@
 module.exports = {
-    "development": {
-      "username": "root",
-      "password": "",
-      "database": "fullsix",
-      "host": "127.0.0.1",
-      "dialect": "mysql",
-      "port": "3306"
-    },
-    "test": {
-      "username": "root",
-      "password": "root",
-      "database": "fullsix",
-      "host": "127.0.0.1",
-      "dialect": "mysql",
-      "port": "3306"
-    },
-    "production": {
-      "username": "root",
-      "password": "root",
-      "database": "fullsix",
-      "host": "127.0.0.1",
-      "dialect": "mysql",
-      "port": "3306"
-    }
-  }
-  
+  development: {
+    username: "root",
+    password: "",
+    database: "fullsix",
+    host: "127.0.0.1",
+    dialect: "mysql",
+    port: "3306",
+  },
+  test: {
+    username: "root",
+    password: "root",
+    database: "fullsix",
+    host: "127.0.0.1",
+    dialect: "mysql",
+    port: "3306",
+  },
+  production: {
+    username: "root",
+    password: "root",
+    database: "fullsix",
+    host: "127.0.0.1",
+    dialect: "mysql",
+    port: "3306",
+  },
+};
