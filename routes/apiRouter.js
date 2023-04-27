0e4e7a6279fcd0e9273f3ecc9b401c93b4cb596a
fares27 <amir.bartilone@gmail.com>
2023-04-27 02:16:55 -0300

    Agrego API de Producto por ID

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const productsApiController = require("../controllers/apis/productsApiController.js");
 const path = require("path");
 
-router.get("/api/products",productsApiController.productAll); // CARRITO DE COMPRAS
+router.get("/api/products",productsApiController.productAll); // API PRODUCTOS -- TRAER TODOS
+
+router.get("/api/products/:id",productsApiController.product); // API PRODUCTOS -- TRAER TODOS
 
 module.exports = router;
