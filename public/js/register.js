cb1c3975abd16086beff3e6763565795c344af75
Marcos923 <marcos.daut@gmail.com>
2023-05-01 16:06:08 -0300

    modificando validaciones frontend

--- public/js/register.js
+++ public/js/register.js
@@ -5,17 +5,17 @@ window.addEventListener("load", function () {
   let userLastname = document.querySelector("[name=lastname]");
   let userEmail = document.querySelector("[name=email]");
   let userPassword = document.querySelector("[name=password]");
-  let userFile = document.querySelector("[name=avatar]");
+  let userFile = document.querySelector("[name=image]");
 
-  console.log(userFirstName, userLastname, userEmail, userPassword, userFile);
+  //   console.log(userFirstName, userLastname, userEmail, userPassword, userFile);
 
   const validateField = (e) => {
     const field = e.target;
     const fieldValue = e.target.value;
-    if (fieldValue.trim().length === 0) {
+    if (fieldValue.trim().length < 2) {
       field.classList.add("invalid");
       field.nextElementSibling.classList.add("error");
-      field.nextElementSibling.innerText = `Debes completar el campo ${field.placeholder}`;
+      field.nextElementSibling.innerText = `El campo ${field.placeholder} es obligatorio y debe tener al menos 2 caracteres`;
     } else {
       field.classList.remove("invalid");
       field.nextElementSibling.classList.remove("error");
@@ -29,7 +29,7 @@ window.addEventListener("load", function () {
     const regexEmail = new RegExp(
       /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i
     );
-    if (fieldValue.trim().length > 5 && !regexEmail.test(fieldValue)) {
+    if (fieldValue.trim().length === 0 || !regexEmail.test(fieldValue)) {
       field.classList.add("invalid");
       field.nextElementSibling.classList.add("error");
       field.nextElementSibling.innerText = "Debes completar un email valido";
@@ -40,12 +40,31 @@ window.addEventListener("load", function () {
     }
   };
 
+  const validatePassword = (e) => {
+    const field = e.target;
+    const fieldValue = e.target.value;
+    const regexPassword = new RegExp(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,15}$/
+    );
+    if (fieldValue.trim().length === 0 || !regexPassword.test(fieldValue)) {
+      field.classList.add("invalid");
+      field.nextElementSibling.classList.add("error");
+      field.nextElementSibling.innerText = `La contraseña deberá tener al menos 8 caracteres, una letra mayusculas, una letra minuscula, un número y un caracter especial`;
+    } else {
+      field.classList.remove("invalid");
+      field.nextElementSibling.classList.remove("error");
+      field.nextElementSibling.innerText = "";
+    }
+  };
+
   userFirstName.addEventListener("blur", validateField);
   userLastname.addEventListener("blur", validateField);
-  userEmail.addEventListener("blur", validateField);
-  userPassword.addEventListener("blur", validateField);
+  //   userEmail.addEventListener("blur", validateField);
 
-  userEmail.addEventListener("input", validateEmail);
+  //   userPassword.addEventListener("blur", validateField);
+  userPassword.addEventListener("blur", validatePassword);
+
+  userEmail.addEventListener("blur", validateEmail);
 
   userFile.addEventListener("change", (e) => {
     const field = e.target;
@@ -55,7 +74,7 @@ window.addEventListener("load", function () {
       field.classList.add("invalid");
       field.nextElementSibling.classList.add("error");
       field.nextElementSibling.innerText = `Los unicos formatos permitidos son ${allowedExt.join(
-        ","
+        ", "
       )}`;
     } else {
       field.classList.remove("invalid");
