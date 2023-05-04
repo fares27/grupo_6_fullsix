311e2d2077be0f13ce9c7ce0ecb8108a23e42a1b
jazminfarias <jazminfarias07@gmail.com>
2023-05-04 16:57:34 -0300

    colores botones segun corresponda en productdetail

--- public/js/productDetail.js
+++ public/js/productDetail.js
@@ -1,14 +1,26 @@
 window.addEventListener("load", function () {
-// Selecciona todos los artículos
-const productos = document.querySelectorAll('.producto-item');
+    // Selecciona todos los artículos
+    const productos = document.querySelectorAll('.producto-item');
+    const contratar = document.querySelectorAll('.boton-producto-item');
 
-// Itera sobre cada tercer artículo y le aplica un color de fondo diferente
-for (let i = 0; i < productos.length; i += 3) {
-  productos[i].classList.add('color-1');
-  if (productos[i + 1]) {
-    productos[i + 1].classList.add('color-2');
-  }
-  if (productos[i + 2]) {
-    productos[i + 2].classList.add('color-3');
-  }
-}});
+    // Itera sobre cada tercer artículo y le aplica un color de fondo diferente
+    for (let i = 0; i < productos.length; i += 3) {
+        productos[i].classList.add('color-1');
+        if (productos[i + 1]) {
+            productos[i + 1].classList.add('color-2');
+        }
+        if (productos[i + 2]) {
+            productos[i + 2].classList.add('color-3');
+        }
+    }
+    for (let i = 0; i < contratar.length; i += 3) {
+        contratar[i].classList.add('color-1');
+        if (contratar[i + 1]) {
+            contratar[i + 1].classList.add('color-2');
+        }
+        if (contratar[i + 2]) {
+            contratar[i + 2].classList.add('color-3');
+        }
+    }
+
+});
