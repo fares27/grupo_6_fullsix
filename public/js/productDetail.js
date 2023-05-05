a6e8fb71247f680add53ceb71f6f6d4c48da0b48
jazminfarias <jazminfarias07@gmail.com>
2023-05-05 16:28:10 -0300

    hover y arreglo boton Crear

--- public/js/productDetail.js
+++ public/js/productDetail.js
@@ -1,7 +1,6 @@
 window.addEventListener("load", function () {
     // Selecciona todos los artículos
     const productos = document.querySelectorAll('.producto-item');
-    const contratar = document.querySelectorAll('.boton-producto-item');
 
     // Itera sobre cada tercer artículo y le aplica un color de fondo diferente
     for (let i = 0; i < productos.length; i += 3) {
@@ -11,15 +10,6 @@ window.addEventListener("load", function () {
         }
         if (productos[i + 2]) {
             productos[i + 2].classList.add('color-3');
-        }
-    }
-    for (let i = 0; i < contratar.length; i += 3) {
-        contratar[i].classList.add('color-1');
-        if (contratar[i + 1]) {
-            contratar[i + 1].classList.add('color-2');
-        }
-        if (contratar[i + 2]) {
-            contratar[i + 2].classList.add('color-3');
         }
     }
 
